b36ca211b0a8014c9ee64786b4e87af6af58cff7
Alexander Siniouguine <sasha@virtoway.com>
2016-07-28 02:13:08 -0700

    added sample test to show pricelists are not returned

--- VirtoCommerce.SearchModule.Tests/SearchFunctionalScenarios.cs
+++ VirtoCommerce.SearchModule.Tests/SearchFunctionalScenarios.cs
@@ -35,6 +35,21 @@ namespace VirtoCommerce.SearchModule.Tests
         }
 
         [Fact]
+        public void Can_return_pricelists()
+        {
+            var evalContext = new Domain.Pricing.Model.PriceEvaluationContext
+            {
+                ProductIds = new[] { "4ed55441810a47da88a483e5a1ee4e94" }
+            };
+
+            var pricingService = GetPricingService();
+            var priceLists = pricingService.EvaluatePriceLists(evalContext);
+            Assert.True(priceLists.Count() > 0);
+            var prices = pricingService.EvaluateProductPrices(evalContext);
+            Assert.True(prices.Count() > 0);
+        }
+    
+        [Fact]
         public void Can_index_demo_data_and_search()
         {
             var scope = "test";
