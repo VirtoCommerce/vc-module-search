60636f031b1a0e4b518260b9261a3790dc216799
Alexander Siniouguine <sasha@virtoway.com>
2016-08-16 16:03:50 -0700

    make sure we wait before executing queries when indexing is involved

--- VirtoCommerce.SearchModule.Tests/SearchHelper.cs
+++ VirtoCommerce.SearchModule.Tests/SearchHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using VirtoCommerce.Domain.Search.Model;
 using VirtoCommerce.Domain.Search.Services;
 
@@ -8,12 +9,16 @@ namespace VirtoCommerce.SearchModule.Tests
     {
         public static void CreateSampleIndex(ISearchProvider provider, string scope)
         {
+            provider.RemoveAll(scope, "catalogitem");
             provider.Index(scope, "catalogitem", CreateDocument("12345", "sample product", "red", 123.23m, 2, new[] { "sony/186d61d8-d843-4675-9f77-ec5ef603fda3", "apple/186d61d8-d843-4675-9f77-ec5ef603fda3" }));
             provider.Index(scope, "catalogitem", CreateDocument("sad121", "red shirt", "red", 10m, 3, new[] { "sony/186d61d8-d843-4675-9f77-ec5ef603fda3", "apple/186d61d8-d843-4675-9f77-ec5ef603fda3" }));
             provider.Index(scope, "catalogitem", CreateDocument("jdashf", "blue shirt", "blue", 23.12m, 8, new[] { "sony/186d61d8-d843-4675-9f77-ec5ef603fda3", "apple/186d61d8-d843-4675-9f77-ec5ef603fda3" }));
             provider.Index(scope, "catalogitem", CreateDocument("32894hjf", "black sox", "black", 243.12m, 10, new[] { "sony/186d61d8-d843-4675-9f77-ec5ef603fda3", "apple/186d61d8-d843-4675-9f77-ec5ef603fda3" }));
             provider.Commit(scope);
             provider.Close(scope, "catalogitem");
+
+            // sleep for index to be commited
+            Thread.Sleep(5000);
         }
 
         private static ResultDocument CreateDocument(string key, string name, string color, decimal price, int size, string[] outlines)
