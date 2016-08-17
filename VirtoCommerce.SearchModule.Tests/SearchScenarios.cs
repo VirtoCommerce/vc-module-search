c7d5106c416d04bcc5587ccdd0aa33164c2f9f0c
Alexander Siniouguine <sasha@virtoway.com>
2016-08-17 15:29:06 -0700

    fix tests

--- VirtoCommerce.SearchModule.Tests/SearchScenarios.cs
+++ VirtoCommerce.SearchModule.Tests/SearchScenarios.cs
@@ -113,19 +113,19 @@ namespace VirtoCommerce.SearchModule.Tests
 
             var results = provider.Search(scope, criteria);
 
-            Assert.True(results.DocCount == 4, string.Format("Returns {0} instead of 4", results.DocCount));
+            Assert.True(results.DocCount == 5, string.Format("Returns {0} instead of 5", results.DocCount));
 
             var redCount = GetFacetCount(results, "Color", "red");
-            Assert.True(redCount == 2, string.Format("Returns {0} facets of red instead of 2", redCount));
+            Assert.True(redCount == 3, string.Format("Returns {0} facets of red instead of 3", redCount));
 
             var priceCount = GetFacetCount(results, "Price", "0_to_100");
             Assert.True(priceCount == 2, string.Format("Returns {0} facets of 0_to_100 prices instead of 2", priceCount));
 
             var priceCount2 = GetFacetCount(results, "Price", "100_to_700");
-            Assert.True(priceCount2 == 2, string.Format("Returns {0} facets of 100_to_700 prices instead of 2", priceCount2));
+            Assert.True(priceCount2 == 3, string.Format("Returns {0} facets of 100_to_700 prices instead of 3", priceCount2));
 
             var sizeCount = GetFacetCount(results, "size", "0_to_5");
-            Assert.True(sizeCount == 2, string.Format("Returns {0} facets of 0_to_5 size instead of 2", sizeCount));
+            Assert.True(sizeCount == 3, string.Format("Returns {0} facets of 0_to_5 size instead of 3", sizeCount));
 
             var sizeCount2 = GetFacetCount(results, "size", "5_to_10");
             Assert.True(sizeCount2 == 1, string.Format("Returns {0} facets of 5_to_10 size instead of 1", sizeCount2)); // only 1 result because upper bound is not included
