921191e0c40e3bb38de8652d8662fb1dc0393987
Alexander Siniouguine <sasha@virtoway.com>
2016-08-23 21:22:04 -0700

    remove c#6 expressions

--- VirtoCommerce.SearchModule.Data/Providers/ElasticSearch.Nest/PropertyHelper.cs
+++ VirtoCommerce.SearchModule.Data/Providers/ElasticSearch.Nest/PropertyHelper.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Reflection;
 using System.Collections;
 using Nest;
@@ -10,7 +9,6 @@ namespace VirtoCommerce.SearchModule.Data.Providers.ElasticSearch.Nest
 {
     class PropertyHelper
     {
-
         public static IProperty InferProperty(Type type)
         {
             type = GetUnderlyingType(type);
@@ -92,95 +90,42 @@ namespace VirtoCommerce.SearchModule.Data.Providers.ElasticSearch.Nest
             return list != null && list.Any();
         }
 
-        internal static bool IsGenericType(this Type type) => type.IsGenericType;
+        internal static bool IsGenericType(this Type type)
+        {
+            return type.IsGenericType;
+        }
 
-        internal static bool IsGenericTypeDefinition(this Type type) => type.IsGenericTypeDefinition;
+        internal static bool IsGenericTypeDefinition(this Type type)
+        {
+            return type.IsGenericTypeDefinition;
+        }
 
-        internal static bool IsValueType(this Type type) => type.IsValueType;
+        internal static bool IsValueType(this Type type)
+        {
+            return type.IsValueType;
+        }
     }
 
     internal static class DotNetCoreTypeExtensions
     {
         internal static bool IsGeneric(this Type type)
         {
-#if DOTNETCORE
-			return type.GetTypeInfo().IsGenericType;
-#else
             return type.IsGenericType;
-#endif
         }
 
         internal static bool IsValue(this Type type)
         {
-#if DOTNETCORE
-			return type.GetTypeInfo().IsValueType;
-#else
             return type.IsValueType;
-#endif
         }
 
         internal static TypeCode GetTypeCode(this Type type)
         {
-#if !DOTNETCORE
             return Type.GetTypeCode(type);
-#else
-			if (type == null)
-				return TypeCode.Empty;
-			else if (type == typeof(bool))
-				return TypeCode.Boolean;
-			else if (type == typeof(char))
-				return TypeCode.Char;
-			else if (type == typeof(sbyte))
-				return TypeCode.SByte;
-			else if (type == typeof(byte))
-				return TypeCode.Byte;
-			else if (type == typeof(short))
-				return TypeCode.Int16;
-			else if (type == typeof(ushort))
-				return TypeCode.UInt16;
-			else if (type == typeof(int))
-				return TypeCode.Int32;
-			else if (type == typeof(uint))
-				return TypeCode.UInt32;
-			else if (type == typeof(long))
-				return TypeCode.Int64;
-			else if (type == typeof(ulong))
-				return TypeCode.UInt64;
-			else if (type == typeof(float))
-				return TypeCode.Single;
-			else if (type == typeof(double))
-				return TypeCode.Double;
-			else if (type == typeof(decimal))
-				return TypeCode.Decimal;
-			else if (type == typeof(System.DateTime))
-				return TypeCode.DateTime;
-			else if (type == typeof(string))
-				return TypeCode.String;
-			else if (type.GetTypeInfo().IsEnum)
-				return GetTypeCode(Enum.GetUnderlyingType(type));
-			else
-				return TypeCode.Object;
-#endif
         }
-
-#if DOTNETCORE
-		internal static bool IsAssignableFrom(this Type t, Type other) => t.GetTypeInfo().IsAssignableFrom(other.GetTypeInfo());
-#endif
 
         internal static bool IsEnumType(this Type type)
         {
-#if DOTNETCORE
-			return type.GetTypeInfo().IsEnum;
-#else
             return type.IsEnum;
-#endif
         }
-
-#if DOTNETCORE
-		internal static IEnumerable<Type> GetInterfaces(this Type type)
-		{
-			return type.GetTypeInfo().ImplementedInterfaces;
-		}
-#endif
     }
 }
