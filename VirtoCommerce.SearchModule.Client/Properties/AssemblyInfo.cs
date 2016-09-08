ebd5f2822cbce52e68722a02d10ec01059960a08
eMazeika <em@virtoway.com>
2016-09-08 17:04:00 +0300

    Client project deleted

--- VirtoCommerce.SearchModule.Client/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("VirtoCommerce.SearchModule.Client")]
-[assembly: AssemblyDescription("VirtoCommerce.Search API client generated from a Swagger doc")]
