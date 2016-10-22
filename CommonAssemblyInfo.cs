d8d4d3341410c1d507ac4b2fd1f8df8f7ca0e51a
artem-dudarev <ad@virtoway.com>
2016-10-23 00:28:58 +0200

    v2.12.6 [publish]

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Search Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
 
-[assembly: AssemblyFileVersion("2.12.5.0")]
-[assembly: AssemblyVersion("2.12.5.0")]
+[assembly: AssemblyFileVersion("2.12.6.0")]
+[assembly: AssemblyVersion("2.12.6.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
