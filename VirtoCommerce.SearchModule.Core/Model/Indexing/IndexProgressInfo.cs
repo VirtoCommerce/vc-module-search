8c62bc8c93c9798fd34dec96af3f81bee0190125
eMazeika <em@virtoway.com>
2016-10-26 18:38:14 +0300

    compile error fix

--- VirtoCommerce.SearchModule.Core/Model/Indexing/IndexProgressInfo.cs
+++ VirtoCommerce.SearchModule.Core/Model/Indexing/IndexProgressInfo.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace VirtoCommerce.SearchModule.Core.Model.Indexing
 {
     public class IndexProgressInfo
     {
         public string Description { get; set; }
-        public long ErrorCount { get; }
+        public long ErrorCount { get { return Errors.Count; } }
         public ICollection<string> Errors { get; set; }
         public long ProcessedCount { get; set; }
         public long TotalCount { get; set; }
