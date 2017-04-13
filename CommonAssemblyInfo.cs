2e84f391ea84858069c34400dc8b282643171909
artem-dudarev <ad@virtoway.com>
2017-04-14 00:40:29 +0200

    Updated copyright

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("VirtoCommerce")]
 [assembly: AssemblyProduct("Virto Commerce Search Module")]
-[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
 
 [assembly: AssemblyFileVersion("2.14.0.0")]
 [assembly: AssemblyVersion("2.14.0.0")]
