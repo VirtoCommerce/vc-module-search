8c30e84872fa5ab5f9cff62a601e9142ba01723b
artem-dudarev <ad@virtoway.com>
2017-04-25 18:09:00 +0200

    Refactored index builders

--- VirtoCommerce.SearchModule.Core/Model/Indexing/IDocumentBuilder.cs
+++ VirtoCommerce.SearchModule.Core/Model/Indexing/IDocumentBuilder.cs
@@ -1,6 +1,6 @@
 ﻿namespace VirtoCommerce.SearchModule.Core.Model.Indexing
 {
-    public interface IDocumentBuilder<in TItem, in TContext>
+    public interface IDocumentBuilder<in TItem>
     {
         /// <summary>
         /// Takes information from item and context and adds fields to document
@@ -9,6 +9,6 @@
         /// <param name="item"></param>
         /// <param name="context"></param>
         /// <returns>False if document should not be indexed</returns>
-        bool UpdateDocument(IDocument document, TItem item, TContext context);
+        bool UpdateDocument(IDocument document, TItem item, object context);
     }
 }
