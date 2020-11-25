88a459e1b1ad22f31d2ec95c7bfa348657fa4cde
Filip Kuksov <vectorfield4@gmail.com>
2020-11-25 14:15:01 +0200

    VP-6081: Copy to clipboard button

--- src/VirtoCommerce.SearchModule.Web/Scripts/blades/index-detail.js
+++ src/VirtoCommerce.SearchModule.Web/Scripts/blades/index-detail.js
@@ -1,44 +1,54 @@
-﻿angular.module('virtoCommerce.searchModule')
-.controller('virtoCommerce.searchModule.indexDetailController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.searchModule.searchIndexation', function ($scope, bladeNavigationService, searchIndexationApi) {
-    var blade = $scope.blade;
+angular.module('virtoCommerce.searchModule')
+    .controller('virtoCommerce.searchModule.indexDetailController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.searchModule.searchIndexation', function ($scope, bladeNavigationService, searchIndexationApi) {
+        var blade = $scope.blade;
 
-    blade.initialize = function (data) {
-        blade.currentEntity = data;
-        blade.isLoading = false;
-    };
+        blade.initialize = function (data) {
+            blade.currentEntity = data;
+            blade.isLoading = false;
+        };
 
-    blade.headIcon = 'fa-search';
+        blade.headIcon = 'fa-search';
 
+        blade.toolbarCommands = [{
+            name: "search.commands.rebuild-index",
+            icon: 'fa fa-recycle',
+            index: 2,
+            executeMethod: function (blade) {
+                searchIndexationApi.index([{ documentType: blade.documentType, documentIds: [blade.currentEntityId] }], function openProgressBlade(data) {
+                    // show indexing progress
+                    var newBlade = {
+                        id: 'indexProgress',
+                        notification: data,
+                        parentRefresh: blade.parentRefresh,
+                        controller: 'virtoCommerce.searchModule.indexProgressController',
+                        template: 'Modules/$(VirtoCommerce.Search)/Scripts/blades/index-progress.tpl.html'
+                    };
+                    bladeNavigationService.showBlade(newBlade, blade.parentBlade || blade);
+                });
+            },
+            canExecuteMethod: function () { return true; },
+            permission: 'search:index:rebuild'
+        }, {
+            name: "search.commands.copy-to-clipboard",
+            icon: 'fa fa-clipboard',
+            executeMethod: function (blade) {
+                var copyElement = document.getElementById("indexData");
+                var range = document.createRange();
+                range.selectNode(copyElement);
+                window.getSelection().removeAllRanges();
+                window.getSelection().addRange(range);
+                document.execCommand('copy');
+                window.getSelection().removeAllRanges();
+            },
+            canExecuteMethod: function () { return true; }
+        }];
 
-    blade.toolbarCommands = [
-     {
-       name: "search.commands.rebuild-index",
-       icon: 'fa fa-recycle',
-       index: 2,
-       executeMethod: function (blade) {
-           searchIndexationApi.index([{ documentType: blade.documentType, documentIds: [blade.currentEntityId]}], function openProgressBlade(data) {
-               // show indexing progress
-               var newBlade = {
-                   id: 'indexProgress',
-                   notification: data,
-                   parentRefresh: blade.parentRefresh,
-                   controller: 'virtoCommerce.searchModule.indexProgressController',
-                   template: 'Modules/$(VirtoCommerce.Search)/Scripts/blades/index-progress.tpl.html'
-               };
-               bladeNavigationService.showBlade(newBlade, blade.parentBlade || blade);
-           });
-       },
-       canExecuteMethod: function () { return true; },
-       permission: 'search:index:rebuild'
-     }
-    ];
+        blade.title = blade.currentEntity.name;
+        blade.subtitle = 'search.blades.index-detail.subtitle';
+        if (!blade.data) {
+            blade.title = 'search.blades.index-detail.title-new';
+            blade.subtitle = undefined;
+        }
 
-    blade.title = blade.currentEntity.name;
-    blade.subtitle = 'search.blades.index-detail.subtitle';
-    if (!blade.data) {
-        blade.title = 'search.blades.index-detail.title-new';
-        blade.subtitle = undefined;
-    }
-
-    blade.initialize(blade.data);
-}]);
+        blade.initialize(blade.data);
+    }]);
