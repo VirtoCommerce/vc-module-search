99beabcd087ed0e78f035581268cf4a01e917992
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2022-03-11 11:25:03 +0200

    Remove redundant class

--- src/VirtoCommerce.SearchModule.Core/Model/IndexingParameters.cs
+++
@@ -1,8 +0,0 @@
-namespace VirtoCommerce.SearchModule.Core.Model
-{
-    public class IndexingParameters
-    {
-        public bool PartialUpdate { get; set; }
-        public bool Reindex { get; set; }
-    }
-}
