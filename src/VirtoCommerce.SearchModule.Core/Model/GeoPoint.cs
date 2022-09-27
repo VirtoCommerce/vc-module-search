b8ef53be0277ae098cea62fccb3f8a45df690efb
OlegoO <zhukoo@gmail.com>
2022-09-27 13:06:43 +0200

    Fix smells

--- src/VirtoCommerce.SearchModule.Core/Model/GeoPoint.cs
+++ src/VirtoCommerce.SearchModule.Core/Model/GeoPoint.cs
@@ -7,6 +7,8 @@ namespace VirtoCommerce.SearchModule.Core.Model
 {
     public class GeoPoint : ValueObject
     {
+        private const int Digits = 10;
+
         public static readonly Regex Regexp = new Regex(@"^([-+]?(?:[1-8]?\d(?:\.\d+)?|90(?:\.0+)?)),\s*([-+]?(?:180(?:\.0+)?|(?:(?:1[0-7]\d)|(?:[1-9]?\d))(?:\.\d+)?))$", RegexOptions.Compiled);
         public GeoPoint()
             : this(0, 0)
@@ -74,8 +76,8 @@ namespace VirtoCommerce.SearchModule.Core.Model
             {
                 result = new GeoPoint
                 {
-                    Latitude = Math.Round(double.Parse(match.Groups[1].Value, NumberStyles.Float, CultureInfo.InvariantCulture), 10) ,
-                    Longitude = Math.Round(double.Parse(match.Groups[2].Value, NumberStyles.Float, CultureInfo.InvariantCulture), 10)
+                    Latitude = Math.Round(double.Parse(match.Groups[1].Value, NumberStyles.Float, CultureInfo.InvariantCulture), Digits) ,
+                    Longitude = Math.Round(double.Parse(match.Groups[2].Value, NumberStyles.Float, CultureInfo.InvariantCulture), Digits)
                 };
             }
             return result;
