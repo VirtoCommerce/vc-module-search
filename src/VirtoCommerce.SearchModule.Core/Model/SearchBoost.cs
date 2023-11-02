508c1209bcab6b330b65b7fcaa2a8ecf1dd7050e
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2023-11-02 08:24:38 +0100

    feat: add SearchBoost collection to SearchRequest

---
+++ src/VirtoCommerce.SearchModule.Core/Model/SearchBoost.cs
@@ -0,0 +1,11 @@
+namespace VirtoCommerce.SearchModule.Core.Model
+{
+    public class SearchBoost
+    {
+        public string FieldName { get; set; }
+
+        public string Value { get; set; }
+
+        public string Preset { get; set; }
+    }
+}
