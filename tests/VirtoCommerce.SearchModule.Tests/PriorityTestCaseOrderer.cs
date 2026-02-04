1787698b2fa996311239cc4c2de8fd1e28472121
Oleg Zhuk <zhukoo@gmail.com>
2026-02-04 08:55:58 +0200

    Fix code review

--- tests/VirtoCommerce.SearchModule.Tests/PriorityTestCaseOrderer.cs
+++ tests/VirtoCommerce.SearchModule.Tests/PriorityTestCaseOrderer.cs
@@ -16,11 +16,33 @@ namespace VirtoCommerce.SearchModule.Tests
         private static int GetPriority(IXunitTestCase testCase)
         {
             // Order the test based on the attribute.
-            var type = Type.GetType(testCase.TestClassName);
+            var type = FindType(testCase.TestClassName);
             var methodInfo = type?.GetMethod(testCase.TestMethodName);
             var attr = methodInfo?.GetCustomAttribute<PriorityAttribute>();
 
             return attr?.Priority ?? 0;
+        }
+
+        private static Type FindType(string typeName)
+        {
+            // First try direct lookup (works for current assembly and mscorlib)
+            var type = Type.GetType(typeName);
+            if (type != null)
+            {
+                return type;
+            }
+
+            // Search all loaded assemblies for cross-assembly test classes
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = assembly.GetType(typeName);
+                if (type != null)
+                {
+                    return type;
+                }
+            }
+
+            return null;
         }
     }
 }
